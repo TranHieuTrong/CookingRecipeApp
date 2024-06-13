ea07177672a49ff026ee0d7f5beb270924c46390
TranHieuTrong <zerusboster1689@gmail.com>
2024-06-13 16:50:45 +0700

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -14,9 +14,8 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = ({navigation}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [userId, setUserId] = useState(''); // Thêm state để lưu _id của người dùng
+  const [email, setEmail] = useState('hchong@gmail.com');
+  const [password, setPassword] = useState('Trong1507@');
   const [error, setError] = useState('');
 
   const next = async () => {
